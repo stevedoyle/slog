7d8633275b18ff2d67a3e6efe10ded4393583b65
Stephen Doyle <stephendoyle75@gmail.com>
2024-08-20 16:05:19 +0100

    Adding 2nd impl of SHAKE to confirm results

--- rust/shake-xof/src/main.rs
+++ rust/shake-xof/src/main.rs
@@ -1,3 +1,7 @@
+use sha3::{
+    digest::{ExtendableOutput, Update, XofReader},
+    Shake128,
+};
 use tiny_keccak::{Hasher, Xof};
 
 /// Experiment to determine if the SHAKE Xof output is the same regardless of the amount of data
@@ -24,7 +28,7 @@ fn shake_xof_single_iter(input: &[u8], output_len: usize) -> Vec<u8> {
     output
 }
 
-fn main() {
+fn using_tiny_keccak() {
     let input = b"Hello, world!";
     let a = shake_xof_2_bytes_per_iter(input, 256);
     let b = shake_xof_single_iter(input, 256);
@@ -34,4 +38,40 @@ fn main() {
     } else {
         println!("The SHAKE Xof output is different depending on the amount of data squeezed from it in one go.");
     }
+}
+
+fn sha3_shake_xof(input: &[u8], output_len: usize) -> Vec<u8> {
+    let mut output = vec![0u8; output_len];
+    let mut hasher = Shake128::default();
+    hasher.update(input);
+    hasher.finalize_xof().read(&mut output);
+    output
+}
+
+fn sha3_shake_xof_multi_iter(input: &[u8], output_len: usize) -> Vec<u8> {
+    let mut output = vec![0u8; output_len];
+    let mut hasher = Shake128::default();
+    hasher.update(input);
+    let mut reader = hasher.finalize_xof();
+    for i in (0..output_len).step_by(2) {
+        reader.read(&mut output[i..i + 2]);
+    }
+    output
+}
+
+fn using_sha3() {
+    let input = b"Hello, world!";
+    let a = sha3_shake_xof(input, 256);
+    let b = sha3_shake_xof_multi_iter(input, 256);
+
+    if a == b {
+        println!("The SHAKE Xof output is the same regardless of the amount of data squeezed from it in one go.");
+    } else {
+        println!("The SHAKE Xof output is different depending on the amount of data squeezed from it in one go.");
+    }
+}
+
+fn main() {
+    using_tiny_keccak();
+    using_sha3();
 }
